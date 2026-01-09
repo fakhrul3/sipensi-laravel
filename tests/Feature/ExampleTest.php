1390a4f65e0a0d6dfe007f55c8df0fcd0ac81534
ejiirizqi <diskarpuskesug@gmail.com>
2026-01-09 09:24:25 +0700

    fix: correct TestCase namespace for feature tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,14 +3,14 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_true_is_true()
+    public function test_true_is_true(): void
     {
         $this->assertTrue(true);
     }
-    
 }
